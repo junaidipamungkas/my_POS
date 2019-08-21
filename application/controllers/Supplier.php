ab8c45a879a7c13b167fd8714a65c2a85028205e
junaidipamungkas <pamungkasjunaidi33@gmail.com>
2019-08-21 22:12:34 +0700

    membuat library template engine

--- application/controllers/Supplier.php
+++ application/controllers/Supplier.php
@@ -1,10 +1,10 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class Dashboard extends CI_Controller {
+class Supplier extends CI_Controller {
 
 	public function index()
 	{
-		$this->template->load('template', 'dashboard'); 
+		$this->template->load('template', 'supplier/supplier_data'); 
 	}
 }
